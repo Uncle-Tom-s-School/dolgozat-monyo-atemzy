a50f6ebd3002f4d44aabd6ac095ef3127bb89323
Atemzy <attizold18@gmail.com>
2025-10-21 12:51:48 +0200

    Update BeerCard.tsx

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -2,7 +2,7 @@ import React from "react"
 const BeerCard: React.FC<{img_src:string,title:string,price:number,inStock:boolean}> = ({img_src,title,price,inStock}) => {
   return (
     <div className='beerCard'>
-      <img src={img_src} alt="" />
+      <img src={img_src} alt={title} />
       <h2><b>{title}</b></h2>
       <p>{price} Ft</p>
       <h3 className={inStock ? "available" : "notAvailable"}>{inStock ? "Raktáron" : "Nincs raktáron"}</h3>
