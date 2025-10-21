2d22eced9e61ea1b80328486a1a6ebcfd8b70afe
Atemzy <attizold18@gmail.com>
2025-10-21 12:43:43 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react"
 import BeerCard from "./components/BeerCard"
 
+interface Beer {
+  name:string,
+  image:string,
+  price:number,
+  available:boolean
+}
+
 const App = () => {
+  const [data,setData] = useState<Beer[]>([])
+
+  useEffect(()=>{
+    fetch("/data.json").then(res=>res.json())
+    .then(res=>{
+      setData(res)
+    })
+  },[])
+
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      {data.map((beer,id)=><BeerCard key={id} title={beer.name} price={beer.price} img_src={beer.image} inStock={beer.available} />)}
     </div>
   )
 }
--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,6 +1,12 @@
-const BeerCard = () => {
+import React from "react"
+const BeerCard: React.FC<{img_src:string,title:string,price:number,inStock:boolean}> = ({img_src,title,price,inStock}) => {
   return (
-    <div className='beerCard'>Beer Card</div>
+    <div className='beerCard'>
+      <img src={img_src} alt="" />
+      <h2><b>{title}</b></h2>
+      <p>{price} Ft</p>
+      <h3 className={inStock ? "available" : "notAvailable"}>{inStock ? "Raktáron" : "Nincs raktáron"}</h3>
+    </div>
   )
 }
 
